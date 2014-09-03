f5eebf652430a528298977cc865a8fd27a66f568
vigo2013 <fw121fw4@163.com>
2014-09-03 15:57:14 +0800

    项目初始化

---
+++ examw-test-app-core/src/com/examw/test/ui/MainActivity.java
@@ -0,0 +1,12 @@
+package com.examw.test.ui;
+
+import android.app.Activity;
+
+/**
+ * 主界面
+ * @author fengwei.
+ * @since 2014年9月3日 下午3:18:19.
+ */
+public class MainActivity extends Activity{
+	
+}
