25c1396dd3782c03a680fb2575810fdc46d28ea5
vigo2013 <fw121fw4@163.com>
2014-10-20 17:38:40 +0800

    ...

--- examw-test-app-core/src/com/examw/test/ui/MainActivity.java
+++ examw-test-app-core/src/com/examw/test/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.examw.test.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * 主界面
@@ -8,5 +9,8 @@ import android.app.Activity;
  * @since 2014年9月3日 下午3:18:19.
  */
 public class MainActivity extends Activity{
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
 }
